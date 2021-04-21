8144318681538452021893ee702fa9ecbe0f8946
Ivan Tikhonov <ivan.tikhonov@intel.com>
2021-04-21 20:34:15 +0300

    update copyright

--- ngraph/frontend/paddlepaddle/include/paddlepaddle_frontend/model.hpp
+++ ngraph/frontend/paddlepaddle/include/paddlepaddle_frontend/model.hpp
@@ -1,5 +1,6 @@
-//*****************************************************************************
-// Copyright 2017-2021 Intel Corporation
+// Copyright (C) 2021 Intel Corporation
+// SPDX-License-Identifier: Apache-2.0
+//
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,7 +13,7 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-//*****************************************************************************
+
 
 #pragma once
 
